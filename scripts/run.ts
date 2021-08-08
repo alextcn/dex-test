acd737f2a6997add6aa1ba8531668d60b512da49
Alex Tkachenko <skyalexx@gmail.com>
2021-08-08 11:31:05 +0300

    Add swap

--- scripts/run.ts
+++ scripts/run.ts
@@ -1,4 +1,5 @@
 import { BigNumber, Contract } from "ethers"
+import { isAddress } from "ethers/lib/utils"
 import { ethers, network } from "hardhat"
 import abi from "../abi.json"
 import dexs from "../dexs.json"
@@ -17,13 +18,19 @@ async function deployToken(): Promise<Contract> {
   return token
 }
 
+async function logBalance(address: string, token: Contract) {
+  console.log(`${address} balances:`)
+  console.log(`- ${ethers.utils.formatEther(await ethers.provider.getBalance(address))} WETH`)
+  console.log(`- ${ethers.utils.formatUnits(await token.balanceOf(address), await token.decimals())} ${await token.symbol()}`)
+}
 
 async function main() {
   const sender = await ethers.provider.getSigner().getAddress()
 
   const router = await ethers.getContractAt(abi.uniRouter, DEX.router)
-  const WETH = await router.WETH()
-  
+  const factory = await ethers.getContractAt(abi.uniFactory, DEX.factory)
+  const WETH = await router.WETH() as string
+
   // deploy USDX token (initial supply is minted to sender)
   const token = await deployToken()
   const symbol = await token.symbol()
@@ -33,35 +40,36 @@ async function main() {
   console.log(`WETH: ${WETH}`)
   console.log(`${await token.symbol()}: ${token.address}`)
   console.log('------')
-
-  console.log(`ETH balance: ${ethers.utils.formatEther(await ethers.provider.getBalance(sender))}`)
-  console.log(`${symbol} balance: ${ethers.utils.formatUnits(await token.balanceOf(sender), decimals)}`)
-  console.log('------')
+  await logBalance(sender, token)
   
-  // create pair and add liquidity 
-  // (if a pool for the passed token and WETH does not exists, one is created automatically, 
-  // and exactly amountTokenDesired/msg.value tokens are added)
-  const amountTokenDesired = ethers.utils.parseUnits('50', decimals)
-  const amountETHDesired = ethers.utils.parseEther('1')
+  // create pair and add liquidity
+  // if a pool for the passed token and WETH does not exists, one is created automatically, 
+  // and exactly amountTokenDesired/msg.value tokens are added
+  console.log('Adding liquidity...')
+  const amountTokenDesired = ethers.utils.parseUnits('500', decimals)
+  const amountETHDesired = ethers.utils.parseEther('10')
   const amountTokenMin = amountTokenDesired
   const amountETHMin = amountETHDesired
   const blockNumber = await ethers.provider.getBlockNumber()
   const deadline = (await ethers.provider.getBlock(blockNumber)).timestamp + (60 * 60)
   let params = { value: amountETHDesired }
-  
-  // TODO: throw on failed txs
-  // approve token transfer first
-  await token.approve(router.address, amountTokenDesired)
-  // add liquidity
-  const tx = await router.addLiquidityETH(
-    token.address, amountTokenDesired, amountTokenMin, amountETHMin, sender, deadline, params
-  )
-  
+  await token.approve(router.address, amountTokenDesired) // approve token transfer first
+  await router.addLiquidityETH(token.address, amountTokenDesired, amountTokenMin, amountETHMin, sender, deadline, params)
+  console.log('Liquidity added')
+  await logBalance(sender, token)
 
-  console.log(`ETH balance: ${ethers.utils.formatEther(await ethers.provider.getBalance(sender))}`)
-  console.log(`${symbol} balance: ${ethers.utils.formatUnits(await token.balanceOf(sender), decimals)}`)
-  // console.log(`tx = ${JSON.stringify(await tx.wait())}`)
+  // swap
+  const amountInETH = ethers.utils.parseEther('1')
+  console.log(`Swapping ${ethers.utils.formatEther(amountInETH)} ETH ...`)
+  await router.swapExactETHForTokens('0', [WETH, token.address], sender, deadline, { value: amountInETH })
+  console.log('Swap succeeded')
+  await logBalance(sender, token)
 
+  // get LP token balance
+  const pairAddress = await factory.getPair(WETH, token.address)
+  const pair = await ethers.getContractAt('ERC20', pairAddress) // get as LP token only
+  const liquidity = await pair.balanceOf(sender)
+  console.log(`LP token balance: ${ethers.utils.formatUnits(liquidity, 18)}`)
 }
 
 main()
